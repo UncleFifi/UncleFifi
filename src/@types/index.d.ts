5bc69d6e7c17f4a69ad00f1fcb6b4ed899248345
felipe ferreira <felipedferreira11@gmail.com>
2021-03-04 23:08:29 -0500

    adding stars

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -4,11 +4,9 @@ type NoState = void
 // used in both prototypes.ts and also in the /src/**/*.ts
 type groupedData<T> = {[key: string]: T}
 
-/*
 interface Window {
     particlesJS(domLocation: string, config: any):void
 }
-*/
 
 // Allowing png imports to work
 declare module "*.png" {
